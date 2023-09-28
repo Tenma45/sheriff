f0a547814a7aa62365cf3a8c8ace4c04c27827d8
Tenma45 <tenten25420@gmail.com>
2023-09-28 11:24:43 +0700

    initial server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io')
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server,{
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 80;
+
+server.listen(PORT, () => {
+  console.log(`WebSocket server is running on port ${PORT}`);
+});
